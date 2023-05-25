b8325e6e9a35bec4e2156f4f7a49e9b89ae34263
juanpagu2101 <guzmanpjpablo@gmail.com>
2023-05-24 19:09:14 -0500

    Actualizacion, ya funcion parte de las funciones en la base de datos

--- src/flyweigh/FlyweightMain.java
+++ src/flyweigh/FlyweightMain.java
@@ -9,52 +9,54 @@ import java.util.Random;
 
 public class FlyweightMain {
     private static final String[] NombreCanciones = new String[1000];
-    private static final String[] NombresListas = new String[4100000];
+    private static final String[] NombresListas = new String[20];
     private static final List<ListaReproduccion> Listas = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println(""
-                + "Proceso de creación de listas de reproducción iniciado,\n"
-                + "este proceso puede ser muy retrasado debido a la gran cantidad de objetos\n"
-                + "que se creará, por favor espere un momento hasta que \n"
-                + "sea notificado de que el proceso ha terminado..");
-
-        Runtime runtime = Runtime.getRuntime();
-        System.out.println("MaxMemory > " + (runtime.maxMemory()/1000000));
+        System.out.println("Proceso de creación de listas de reproducción iniciado...");
         FabricaCanciones.HabilitarFlyweight = true;
         InicializarArreglos();
         CrearListaDinamica();
-        System.out.println("Total Listas > " + Listas.size());
-
-        long memoryUsed = runtime.totalMemory() - runtime.freeMemory();
-        System.out.println("Memoria Usada => " + (memoryUsed / 1000000));//
+        System.out.println("Total Listas: " + Listas.size());
+        ImprimirListas();
     }
 
     private static void CrearListaDinamica() {
         Random random = new Random();
         int p = 0;
         for (int c = 0; c < NombresListas.length; c++) {
-            ListaReproduccion playList = new ListaReproduccion(NombresListas[c]);
-            for (int i = 0; i < 10; i++) {
-                int song = random.nextInt(NombreCanciones.length);
-                playList.addCancion(NombreCanciones[song]);
-            }
-            Listas.add(playList);
-            if(c!=0 && (c+1)%(NombresListas.length/10)==0){
-                p+=10;
-                System.out.println("Finalizado "+ p +"%");
-                System.out.println("Listas Creadas " + Listas.size());
+            ListaReproduccion playList = ListaReproduccion.getFromDatabase(NombresListas[c]);
+            if (playList == null) {
+                playList = new ListaReproduccion(NombresListas[c]);
+                for (int i = 0; i < 10; i++) {
+                    int song = random.nextInt(NombreCanciones.length);
+                    playList.addCancion(NombreCanciones[song]);
+                }
+                playList.saveToDatabase(); // Guardar la lista de reproducción en la base de datos
+                Listas.add(playList);
+                if (c != 0 && (c + 1) % (NombresListas.length / 10) == 0) {
+                    p += 10;
+                    System.out.println("Finalizado " + p + "%");
+                    System.out.println("Listas Creadas: " + Listas.size());
+                }
             }
         }
     }
 
     private static void InicializarArreglos() {
         for (int c = 0; c < NombreCanciones.length; c++) {
-            NombreCanciones[c] = "Song " + (c + 1);
+            NombreCanciones[c] = "Canción " + (c + 1);
         }
 
         for (int c = 0; c < NombresListas.length; c++) {
-            NombresListas[c] = "PlayList " + (c + 1);
+            NombresListas[c] = "Lista " + (c + 1);
+        }
+    }
+
+    private static void ImprimirListas() {
+        for (ListaReproduccion playlist : Listas) {
+            playlist.printPlaylist();
+            System.out.println("-----------");
         }
     }
 }
